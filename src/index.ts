020129f7c47c11c6e24056925a9074de54cbd8d9
Maher-Guerfali <maher.guerfali@gmail.com>
2025-05-30 11:06:40 +0100

    43edlrfefllffde4

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@
  * If you're self-hosting (without Arena), you can manually instantiate a
  * Colyseus Server as documented here: 👉 https://docs.colyseus.io/server/api/#constructor-options
  */
-import dotenv from 'dotenv';
+
 
 import { listen } from "@colyseus/tools";
 
 // Import arena config
 import app from "./app.config";
-dotenv.config();
+
 // Create and listen on 2567 (or PORT environment variable.)
 listen(app);
